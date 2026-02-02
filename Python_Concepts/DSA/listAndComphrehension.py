dbd2c31f9cc87c4d7e319d32423986b277fe57da
kannika07112 <kkannikarao07@gmail.com>
2026-02-02 18:13:28 +0530

    list concept

---
+++ Python_Concepts/DSA/listAndComphrehension.py
@@ -0,0 +1,11 @@
+lst=[]
+print("Type: ",type(lst))
+names= ["John","Alice", "Bob"]
+print("Names in the list: ",names)
+
+mixed_list=[5,"Hello World",99.9, False]
+print("Mixed List is also supported: ",mixed_list)
+
+### Accessing list elements
+fruits = ["Apple","banana","cherry","kiwi","gauva"]
+print(fruits[0])
